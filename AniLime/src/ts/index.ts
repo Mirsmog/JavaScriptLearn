a3c7ef9b9437c898fef5371971e6ee88f848fd22
Silve <tanharka.tyuchiev4@mail.ru>
2023-04-01 12:17:23 +0500

    finish layout without adaptive

--- AniLime/src/ts/index.ts
+++ AniLime/src/ts/index.ts
@@ -1,5 +1,6 @@
 export {}
 
+
 async function getUsers(): Promise<unknown>{
   const response = await fetch('https://kitsu.io/api/edge/anime?page[limit]=5&page[offset]=0');
   if (response.ok) {
@@ -8,5 +9,32 @@ async function getUsers(): Promise<unknown>{
   }
 }
 
-const animeInfo = await getUsers();
-console.log(animeInfo);
+const anime = await getUsers()
+
+
+console.log(anime)
+
+// function addAnimeToHtml(){
+//     const wrapper = document.querySelector('#anime__wrapper')
+//   for(let i=0; i<10; i++){
+//     let animeCard = `<li class="anime-card">
+//             <a href="#" class="anime-img">
+//               <img src="${anime.attributes.posterImage.tiny}" alt="anime-img">
+//             </a>
+//             <a href="#" class="anime__name">
+//               <h3>The Rising of the Shield Hero</h3>
+//             </a>
+//             <a href="#" class="anime__series">Series</a>
+//             <div class="anime-stars">
+//               <img src="./src/img/star.svg" alt="img-start" class="img-star">
+//               <div class="anime-star__number">
+//                 9,1
+//               </div>
+//             </div>
+//           </li>
+// `
+//     wrapper?.appendChild('22')
+//   }
+// }
+
+// addAnimeToHtml()
